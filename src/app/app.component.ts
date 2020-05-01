1e2f3363a6470fc6509547b2c4fe00ebf56a61c3
Sriram Venkataramani <yuhup2edy@Srirams-MacBook-Pro.local>
2020-05-01 17:20:52 -0400

    Added formbuilder changes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { FormGroup, FormControl } from '@angular/forms';
+import { FormBuilder } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -7,15 +8,51 @@ import { FormGroup, FormControl } from '@angular/forms';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
+
+  constructor(private fb : FormBuilder){}
+
   title = 'Reactive or Model Driven Approach';
-  registrationForm = new FormGroup({
-    requester : new FormControl('venkatS'),
-    password : new FormControl(''),
-    confirmPassword : new FormControl(''),
-    address : new FormGroup({
-      city : new FormControl('Belle Mead'),
-      state : new FormControl('New Jersey'),
-      zipCode : new FormControl('08502')
-    })
-  });
+  // registrationForm = new FormGroup({
+  //   requester : new FormControl('venkatS'),
+  //   password : new FormControl(''),
+  //   confirmPassword : new FormControl(''),
+  //   address : new FormGroup({
+  //     city : new FormControl('Belle Mead'),
+  //     state : new FormControl('New Jersey'),
+  //     zipCode : new FormControl('08502')
+  //   })
+  // });
+
+  registrationForm = this.fb.group
+  (
+    {
+      requester : ['Sriram'],
+      password  : [''],
+      confirmPassword : [''],
+      address : this.fb.group
+      (
+        {
+          city : ['Fairfax'],
+          state : ['Virginia'],
+          zipCode : ['22033']
+        }
+      )
+    }
+  );
+
+  loadAPI()
+  {
+    this.registrationForm.setValue(
+      //* if you need to load only partial data, use the .patchValue method
+      {
+        requester : 'Venkataramani',
+        password : 'abcnews',
+        confirmPassword : 'abcnews',
+        address : {
+          city : 'Chennai',
+          state : 'Tamil Nadu',
+          zipCode : '600091'
+        }
+      });
+  }
 }
